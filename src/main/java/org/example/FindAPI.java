60fb676b01382ed281b0b87ccc2e7b91b122b481
Geon.C <chlrjs1324@gmail.com>
2023-11-10 16:56:54 +0900

    기본 & 문자 자료형 실습

---
+++ src/main/java/org/example/FindAPI.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javax.lang.model.SourceVersion;
+
+public class FindAPI {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("a = " + a);
+
+        /*
+        * "APPLE" 문자열 저장 후 출력
+        * String은 참조형 자료형이지만 컴파일러에 걸러지지 않는다.
+        * 자바에서 기본 API로(External Libraries) String을 제공해주기 때문에
+        * 컴파일러가 해당 참조형이 있는 클래스에서 탐색함
+        */
+        String word = "APPLE";
+        System.out.println("word = " + word);
+        System.out.println(word.toLowerCase());
+    }
+}
