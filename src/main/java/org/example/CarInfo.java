9bfc5c18a4a2f4f74c0204eef70a69f3f44f0648
Geon.C <chlrjs1324@gmail.com>
2023-11-10 16:40:25 +0900

    [course1]part 1실습

---
+++ src/main/java/org/example/CarInfo.java
@@ -0,0 +1,25 @@
+package org.example;
+
+
+public class CarInfo {
+    public static void main(String[] args) {
+        // 자동차의 정보를 저장하는 클래스
+        String manufacturing_company = "BMW";
+        System.out.println("manufacturing_company = " + manufacturing_company);
+
+        String model = "BMW528i";
+
+        long distance = 100000L;
+
+        int price = 9000000;
+
+        char type = 'A';
+
+        boolean auto = true;
+
+        int year = 2007;
+
+        float gasmi = 12.5f;
+
+    }
+}
